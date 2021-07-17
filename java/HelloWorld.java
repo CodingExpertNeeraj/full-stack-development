4e1aab04aced4553a4e5079de5042cd37eb239a6
Neeraj Tiwari <nt98924@gmail.com>
2021-07-17 11:28:07 +0530

    my proggram

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld
 {
 public static void main(String[] args) {
     System.out.println("Hello World");
+
 }
 }
---
+++ java/Practice.java
@@ -0,0 +1,5 @@
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Hey this is my Practice");
+    }
+}
