ddf7908aca550b017f1908f55dc391d5203e69fb
Rachit Kumar Rastogi <rachitrastogi777@gmail.com>
2021-07-28 11:08:18 -0400

    Puja organization class

---
+++ java/week3/SaurabhOrganizer.java
@@ -0,0 +1,6 @@
+public class SaurabhOrganizer {
+    public static void main(String[] args) {
+        PujaOrganization myPuja = new PujaOrganization("26/07/2021", 100);
+        myPuja.pujaInvite();
+    }
+}
