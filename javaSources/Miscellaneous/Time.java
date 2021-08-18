17d5692c73d03e6b8ce2aa4635588485dde87a06
Neeraj Tiwari <nt98924@gmail.com>
2021-08-18 13:49:02 +0530

    if else

---
+++ javaSources/Miscellaneous/Time.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+    public static void main(String[] args) {
+     Scanner Greet = new Scanner(System.in);
+     System.out.println("enter the time")
+     int a = greet.nextInt();
+     
+
+
+}
