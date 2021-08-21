1a83311aff42623ec9d9949025489352e2591b5f
Neeraj Tiwari <nt98924@gmail.com>
2021-08-21 22:14:56 +0530

    while

--- javaSources/Miscellaneous/Vote.java
+++ javaSources/Miscellaneous/Vote.java
@@ -3,17 +3,17 @@ public class Vote{
 
 
     public static void main(String[] args) {
-        int c = 18;
+      
 
         Scanner myScanner = new Scanner (System.in);
         System.out.println("pls enter the age");
-        c = myScanner.nextInt();
+        int c = myScanner.nextInt();
         if(c >= 18 ){
-            System.out.println("you are iligible to vote ");
+            System.out.println("you are eligible to vote ");
         }
         else if(c < 18){
 
-            System.out.println("you are not iligible to vote");
+            System.out.println("you are not eligible to vote");
         }
     }
 }
---
+++ javaSources/Miscellaneous/While.java
@@ -0,0 +1,16 @@
+public class While{
+    
+    public static void main(String[] args) {
+        int z = 1;
+        while(z <= 20)
+        {
+            z++;
+        
+            System.out.println(z);
+        }
+        
+        
+    }
+
+    }
+    
