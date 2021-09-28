7c8501501088e44f3a876af11bd554d792e1fb61
Neeraj Tiwari <nt98924@gmail.com>
2021-09-28 17:26:58 +0530

    math class

--- javaSources/Practice/MathClass.java
+++ javaSources/Practice/MathClass.java
@@ -2,7 +2,7 @@ public class MathClass {
     public static void main(String[] args) {
         //int a = (int) Math.abs(-4);
 		int a = (int) Math.max(150, 25);
-	     //int a = (int) Math.min(34, 54);
+	     int b = (int) Math.min(34, 54);
 		//int a = (int) Math.sqrt(16);
 		//int a = (int) Math.random();
 		System.out.println(a);
