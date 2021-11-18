3f716c9da7cfcd49d42393dba99b235ceed94cd9
Neeraj Tiwari <nt98924@gmail.com>
2021-11-18 21:56:17 +0530

    find the longest path

--- javaSources/Miscellaneous/MyProggram.java
+++ javaSources/Miscellaneous/MyProggram.java
@@ -3,7 +3,10 @@ import javax.lang.model.util.ElementScanner14;
 class MyProggram
  {
     public static void main(String[] args) {
-        int no = 10; no % 2 == 0)System.out.println("No is Even ");
-       else System.out.println("No is  odd");
+        int no = 10; no % 2 == 0);
+        System.out.println("No is Even ");
+       else{
+        System.out.println("No is  odd");
     }
 }
+ }
