6784d868f62b01cf62ecce39dd8be60460e6bca9
Neeraj Tiwari <nt98924@gmail.com>
2021-11-22 18:38:50 +0530

    bubble sort

---
+++ javaSources/Miscellaneous/Stack.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Stack {
+    public static void main(String[] args) {
+
+        Stack String; Item = new[]; Stack String<>;
+		
+		Item.add("Coat");
+		Item.add("Pant");
+		Item.add("shirt");
+		Item.add("Tie");
+		Item.add("shoes");
+		
+		System.out.println(Item);
+
+        
+    }
+}
